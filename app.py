d8d1ff06197d7140419e29bfe0e5ee1bee71ecdd
Vasudha Gupta <vasudhathinks@gmail.com>
2019-01-04 14:32:07 -0500

    Updates app.py and html files

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 from flask import Flask, render_template, request, redirect
+import json
+import requests
+import pandas as pd
+
+
+# Load key (Sort how to make private)
+def load_key(key_file):
+    with open(key_file) as json_data:
+        keys_dict = json.load(json_data)
+    return keys_dict['api_key']
+
 
 app = Flask(__name__)
 
+app.vars = {}
+api_key = load_key('key.json')
+
 
 @app.route('/')
+def main():
+    return redirect('/index')
+
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/plot', methods=['POST'])
+def plot():
+    return 'plot to be added'
+
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
 
 
 if __name__ == '__main__':

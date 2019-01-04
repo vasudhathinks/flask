99691d17a940512708bdfec42cb1ad88d2734146
Vasudha <17420940+vasudhathinks@users.noreply.github.com>
2019-01-03 23:37:14 -0500

    updates port for heroku

--- app.py
+++ app.py
@@ -11,4 +11,6 @@ def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+#   app.run(port=33507)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

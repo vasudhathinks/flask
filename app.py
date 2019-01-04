7c80c03c12b2613b69a5ebdd5361c454b526495a
Vasudha Gupta <vasudhathinks@gmail.com>
2019-01-04 17:25:23 -0500

    Changes to dynamic port

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def resources():
 
 
 if __name__ == '__main__':
-    app.run(port=33507)
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(port=33507)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     # app.run()

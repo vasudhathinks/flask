0242128f019a0e8b9fa32e499fe46321715282db
Vasudha Gupta <vasudhathinks@gmail.com>
2019-01-04 11:51:08 -0500

    Formats app.py

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(port=33507)
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
